ccafb027b383344f1c5031c1e0148b691fb96773
Franco Rutigliano <francorutigliano11@gmail.com>
2023-07-25 21:53:52 -0300

    Previsualzando mi primer modelo DB - Mostrando projectos (funcionando)

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,5 +6,5 @@ from django.db.models.fields.files import ImageField
 class Project(models.Model):
     title = CharField(max_length=100)
     description = CharField(max_length=200)
-    image = ImageField(upload_to='portfolio/images')
+    image = ImageField(upload_to='portfolio/images/')
     url = URLField(blank=True)
