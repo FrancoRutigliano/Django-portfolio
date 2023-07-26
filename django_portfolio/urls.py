68ac05ad4399b509fa05ae58d4e28bf694bbadc5
Franco Rutigliano <francorutigliano11@gmail.com>
2023-07-25 23:46:43 -0300

    Virtualizacion de blog trayendo posts desde la base de datos dependiendo de id (funcionando)

--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -7,7 +7,8 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.home, name='Home')
+    path('', views.home, name='Home'),
+    path('blog/', include('blog.urls'))
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
